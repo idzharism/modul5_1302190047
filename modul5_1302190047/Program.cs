3580e928c2b71852cc91e93bf67257f8cdd42b42
idzharism <idzharism@student.telkomuniversity.ac.id>
2022-03-26 13:29:23 +0700

    menambahkan dua class SayaTubeVideo dan SayaTubeUser

--- modul5_1302190047/Program.cs
+++ modul5_1302190047/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class SayaTubeVideo
+{
+    int id;
+    string title;
+    int playCount;
+
+
+    public SayaTubeVideo()
+    {
+        Random generate = new Random();
+        this.id = generate.Next(0, 100000);
+        this.playCount = 0;
+    }
+
+    public SayaTubeVideo()
+    {
+        Random generate = new Random();
+        this.id = generate.Next(0, 100000);
+        this.playCount = 0;
+    }
+
+    public void IncreaseplayCount(int angka)
+    {
+        playCount = playCount + angka;
+    }
+
+    public void printVideoDetails()
+    {
+        Console.WriteLine("title: " + title);
+        Console.WriteLine("id: " + id);
+        Console.WriteLine("playCount:" + playCount);
+    }
+
+    internal int getPlayCount()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class SayaTubeUser
+{
+    private IEnumerable<SayaTubeVideo> uploadedVideos;
+
+    public int GetTotalVideoPlayCount()
+    {
+        int total = 0;
+        foreach (SayaTubeVideo video in this.uploadedVideos)
+        {
+            total += video.getPlayCount();
+        }
+        return total;
+    }
+}
